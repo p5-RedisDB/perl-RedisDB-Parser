bc7318598cc92bbb68d7fe765675f047a1a18210
Pavel Shaydo <zwon@cpan.org>
2013-11-23 19:10:32 +0800

    module skeleton

---
+++ lib/RedisDB/Parser.pm
@@ -0,0 +1,48 @@
+package RedisDB::Parser;
+use strict;
+use warnings;
+our $VERSION = "0.01";
+$VERSION = eval $VERSION;
+
+=head1 NAME
+
+RedisDB::Parser - Perl extension ...
+
+=head1 VERSION
+
+0.01
+
+=head1 SYNOPSIS
+
+    use RedisDB::Parser;
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
+
+1;
+
+__END__
+
+=head1 BUGS
+
+Please report any bugs or feature requests via GitHub bug tracker at
+L<http://github.com/trinitum/perl-RedisDB-Parser/issues>.
+
+=head1 AUTHOR
+
+Pavel Shaydo C<< <zwon at cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2013 Pavel Shaydo
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
